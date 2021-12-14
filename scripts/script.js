7982e88e289041cf9084362c8a9409a509d4e7a4
NoorMeijns <94113257+NoorMeijns@users.noreply.github.com>
2021-12-14 14:10:26 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,16 @@
-var fontsizeKnop = document.querySelector(".font-size");
+var fontsizeKnop = document.querySelector(".font-bigger");
+var fontsizeKnop1 = document.querySelector(".font-smaller");
 var main = document.querySelector(".tweedepagina");
 
-fontsizeKnop.addEventListener("click", fontVergroten);
+fontsizeKnop.addEventListener('click', fontVergroten);
+fontsizeKnop1.addEventListener('click', fontVerkleinen);
+
 
 function fontVergroten() {
     console.log(fontsizeKnop);
     main.style.setProperty("font-size", "1.5em");
+}
+
+function fontVerkleinen() {
+    main.style.setProperty("font-size", "1.2em");
 }
