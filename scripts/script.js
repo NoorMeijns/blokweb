0c4153a137f48b204b802f6e3b12ebd293b032ea
NoorMeijns <94113257+NoorMeijns@users.noreply.github.com>
2021-12-14 14:03:47 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
-// JavaScript Document
+var fontsizeKnop = document.querySelector(".font-size");
+var main = document.querySelector(".tweedepagina");
+
+fontsizeKnop.addEventListener("click", fontVergroten);
+
+function fontVergroten() {
+    console.log(fontsizeKnop);
+    main.style.setProperty("font-size", "1.5em");
+}
